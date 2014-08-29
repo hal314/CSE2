328289d4a8380375708612f4837b1003ceeed3a0
hal314 <hal314@lehigh.edu>
2014-08-29 13:51:39 +0000

    WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+//Hao Lu
+//HW1
+//Welcome Class Program
+//
+//first compile the program
+//    javac WelcomeClass.java
+//run the program
+//    java WelcomeClass
+
+//define a class
+public class WelcomeClass{
+//add main method    
+    public static void main(String[] args) {
+        System.out.println("  -----------       ");
+        System.out.println("  | WELCOME |       ");
+        System.out.println("  -----------       ");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-H--A--L--3--1--4-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+    }
+}
