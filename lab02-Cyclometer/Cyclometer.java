eb1a2c96715d07eeee40e8434a3cba8696cb60ea
hal314 <hal314@lehigh.edu>
2014-09-05 13:35:06 +0000

    updated Cyclometer

--- lab02-Cyclometer/Cyclometer.java
+++ lab02-Cyclometer/Cyclometer.java
@@ -4,15 +4,15 @@
 
 public class Cyclometer{
   public static void main(String [] args){
-  int secsTrip1=480;//
-  int secsTrip2=3220;//
-  int countsTrip1=1561;//
-  int countsTrip2=9037;//
-  double wheelDiameter=27.0,
-  PI=3.14159,//
-  feetPerMile=5280,//
-  inchesPerFoot=12,//
-  secondsPerMinute=60;//
+  int secsTrip1=480;//time used in trip1
+  int secsTrip2=3220;//time used in trip 2
+  int countsTrip1=1561;//the counts of rotation in trip1
+  int countsTrip2=9037;//the counts of rotation in trip2
+  double wheelDiameter=27.0,//the diameter of the wheel
+  PI=3.14159,//PI value
+  feetPerMile=5280,//the constant used to convert foot to mile
+  inchesPerFoot=12,//the constant used to convert inch to foot
+  secondsPerMinute=60;//the constant used to convert second to minute
 double distanceTrip1,distanceTrip2,totalDistance;
 
 System.out.println("Trip 1 took "+
